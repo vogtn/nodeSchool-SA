6b2b1df80efb203f1fdd9f847e6f47f2a94501af
vogtn <vogtn@uw.edu>
2016-07-11 13:10:45 -0700

    pipe lines of alternating case

--- lines.js
+++ lines.js
@@ -1,2 +1,21 @@
 var through = require("through2");
 var split = require("split");
+var tr = through(write);
+var lineCount = 0;
+
+  function write(buffer,_,next){
+    var line = buffer.toString();
+    this.push(lineCount % 2 === 0
+    ? line.toLowerCase() + '\n'
+    : line.toUpperCase() + '\n'
+);
+lineCount++;
+    next();
+  }
+  function end (done){
+    done();
+  }
+  process.stdin
+  .pipe(split())
+  .pipe(tr)
+  .pipe(process.stdout);
